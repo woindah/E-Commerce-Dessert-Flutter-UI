f6ae8bb69125d9405c0592632418794235ee3b1f
woindah <102006082+woindah@users.noreply.github.com>
2022-12-08 15:11:30 +0700

    dessert

---
+++ lib/models/Product.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+
+class Product {
+  final int id;
+  final String title, description;
+  final List<String> images;
+  final List<Color> colors;
+  final double rating, price;
+  final bool isFavourite, isPopular;
+
+  Product({
+    required this.id,
+    required this.images,
+    required this.colors,
+    this.rating = 0.0,
+    this.isFavourite = false,
+    this.isPopular = false,
+    required this.title,
+    required this.price,
+    required this.description,
+  });
+}
+
+// Our demo Products
+
+List<Product> demoProducts = [
+  Product(
+    id: 1,
+    images: [
+      "assets/images/cup cake 1.png",
+      "assets/images/cup cake 2.png",
+    ],
+    colors: [
+      Color(0xFFF6625E),
+      Color(0xFF836DB8),
+      Color(0xFFDECB9C),
+      Colors.white,
+    ],
+    title: "Cup Cake Ter Enak Varian Rasa",
+    price: 20.00,
+    description: description,
+    rating: 4.8,
+    isFavourite: true,
+    isPopular: true,
+  ),
+  Product(
+    id: 2,
+    images: [
+      "assets/images/macaron 1.png",
+    ],
+    colors: [
+      Color(0xFFF6625E),
+      Color(0xFF836DB8),
+      Color(0xFFDECB9C),
+      Colors.white,
+    ],
+    title: "Macaron Mini Box Isi 6 pcs",
+    price: 50.5,
+    description: description,
+    rating: 4.1,
+    isPopular: true,
+  ),
+  Product(
+    id: 3,
+    images: [
+      "assets/images/donat 1.png",
+      "assets/images/donat 2.png",
+      "assets/images/donat 3.png",
+    ],
+    colors: [
+      Color(0xFFF6625E),
+      Color(0xFF836DB8),
+      Color(0xFFDECB9C),
+      Colors.white,
+    ],
+    title: "Donat Kentang Varian Rasa",
+    price: 36.55,
+    description: description,
+    rating: 4.1,
+    isFavourite: true,
+    isPopular: true,
+  ),
+  Product(
+    id: 4,
+    images: [
+      "assets/images/wireless headset.png",
+    ],
+    colors: [
+      Color(0xFFF6625E),
+      Color(0xFF836DB8),
+      Color(0xFFDECB9C),
+      Colors.white,
+    ],
+    title: "Logitech Head",
+    price: 20.20,
+    description: description,
+    rating: 4.1,
+    isFavourite: true,
+  ),
+];
+
+const String description =
+    "BARANG LANGSUNG DARI PABRIK, EXP masih lama (karena selalu fresh dari pabrik) dan ber BPOM & label HALAL MUI …";
